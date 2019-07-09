41ebf4d64eae9473be52ab6cd7ca3614b7f298b5
gituser <gituser@192.168.156.1>
2019-07-10 04:06:00 +0800

    修改端口8087

--- src/main/java/com/mayikt/controller/App.java
+++ src/main/java/com/mayikt/controller/App.java
@@ -12,7 +12,7 @@ public class App {
 
 	@RequestMapping("/")
 	public String index() {
-		return "每特教育独创基于 Jenkins+docker部署SpringBoot项目QQ644064779.....www.mayikt.com";
+		return "大家好  我是渣渣辉...是兄弟就来砍我....";
 	}
 
 	public static void main(String[] args) {
